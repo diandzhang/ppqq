8ad1ebf4bddafe8952006ff9f142afb5c32e52e6
diandzhang <diandzhang@gmail.com>
2024-02-15 20:33:59 +0800

    Add files via upload

---
+++ update.py
@@ -0,0 +1,27 @@
+import os
+import re
+file_path="adguard_rules"
+def geturl(msg):
+    msgs=msg.split("'")
+    for line in msgs:
+        if "0.html" in line :
+            # print(line)
+            protocodl =re.findall(r'(?<=://)[^/]+', line)
+            for linez in protocodl: 
+               return linez
+lines = set()
+if os.path.exists(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            lines.add(line.strip())
+
+with open("temp.xml", encoding='utf-8') as markup_file:
+    for line in markup_file:
+        lines.add("||" +geturl(line) + "^")
+
+with open(file_path, 'w') as file:
+    file.write('\n'.join(lines))
+
+
+
+      
